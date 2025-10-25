4d73b6cac6ad755501e2411f8461f95f94ac11d8
Mystic <150622366+ammystic@users.noreply.github.com>
2025-10-25 03:38:43 +0100

    Add offsets for various game components

---
+++ offsets.hpp
@@ -0,0 +1,61 @@
+//     __  ___           __  _     
+//    /  |/  /_  _______/ /_(_)____
+//   / /|_/ / / / / ___/ __/ / ___/
+//  / /  / / /_/ (__  ) /_/ / /__  
+// /_/  /_/\__, /____/\__/_/\___/  
+//        /____/                   
+//
+//Offsets by github.com/ammystic
+//date: 2025-10-25 // 03:25 
+
+// version-30215da31a3b42e2
+
+
+
+    inline constexpr uintptr_t VisualEnginePointer = 0x7109258;
+    inline constexpr uintptr_t DataModelDeleterPointer = 0x73A7080;
+    inline constexpr uintptr_t TaskSchedulerPointer = 0x778C100;
+    inline constexpr uintptr_t FakeDataModelPointer = 0x73A7088;
+    inline constexpr uintptr_t FakeDataModelToDataModel = 0x1C0;
+    inline constexpr uintptr_t VisualEngine = 0x10;
+    inline constexpr uintptr_t Name = 0x80;
+    inline constexpr uintptr_t NameSize = 0x10;
+    inline constexpr uintptr_t PlaceId = 0x198;
+    inline constexpr uintptr_t GameId = 0x190;
+    inline constexpr uintptr_t Workspace = 0x178;
+    inline constexpr uintptr_t Parent = 0x50;
+    inline constexpr uintptr_t ClassDescriptor = 0x18;
+    inline constexpr uintptr_t ClassDescriptorToClassName = 0x8;
+    inline constexpr uintptr_t Children = 0x60;
+    inline constexpr uintptr_t ChildrenEnd = 0x8;
+    inline constexpr uintptr_t LocalPlayer = 0x130;
+    inline constexpr uintptr_t ModelInstance = 0x360;
+    inline constexpr uintptr_t FrameSizeY = 0x500;
+    inline constexpr uintptr_t FrameSizeX = 0x4d8;
+    inline constexpr uintptr_t AttributeList1 = 0x40;
+    inline constexpr uintptr_t AttributeList2 = 0x18;
+    inline constexpr uintptr_t AttributeToNext = 0x58;
+    inline constexpr uintptr_t AttributeToValue = 0x70;
+    inline constexpr uintptr_t Value = 0xd0;
+    inline constexpr uintptr_t Camera = 0x410;
+    inline constexpr uintptr_t CameraPos = 0x11c;
+    inline constexpr uintptr_t CameraRotation = 0xf8;
+    inline constexpr uintptr_t CameraSubject = 0xe8;
+    inline constexpr uintptr_t Gravity = 0x958;
+    inline constexpr uintptr_t UserId = 0x298;
+    inline constexpr uintptr_t FOV = 0x15c;
+    inline constexpr uintptr_t Team = 0x270;
+    inline constexpr uintptr_t TeamColor = 0xd0;
+    inline constexpr uintptr_t CreatorId = 0x188;
+    inline constexpr uintptr_t Health = 0x194;
+    inline constexpr uintptr_t MaxHealth = 0x1b4;
+    inline constexpr uintptr_t HumanoidStateId = 0x20;
+    inline constexpr uintptr_t JumpPower = 0x1b0;
+    inline constexpr uintptr_t WalkSpeed = 0x1d4;
+    inline constexpr uintptr_t WalkSpeedCheck = 0x3a0;
+    inline constexpr uintptr_t Primitive = 0x170;
+[-] Could not find BasePart position in Primitive.
+    inline constexpr uintptr_t OnDemandInstance = 0x20;
+    inline constexpr uintptr_t JobId = 0x138;
+    inline constexpr uintptr_t MoveDirection = 0x158;
+    inline constexpr uintptr_t Value = 0xd0;
